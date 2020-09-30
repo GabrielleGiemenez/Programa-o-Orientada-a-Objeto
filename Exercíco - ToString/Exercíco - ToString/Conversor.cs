131a61de8e40f1bd0f7f41ca469a20bb251bedf0
Gabi <Gabi@DESKTOP-K0ODT6V>
2020-09-30 17:41:51 -0300

    lição

---
+++ Exercíco - ToString/Exercíco - ToString/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercíco___ToString
+{
+    class Conversor
+    {
+        public static double Iof = 6;
+
+        public static double ConversorProReal(double valor, double cotação)
+        {
+            double total = valor * cotação;
+            return total + total * Iof / 100;
+        }
+    }
+}
---
+++ Exercíco - ToString/Exercíco - ToString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercíco___ToString
+{
+    class Program
+
+    static void main(string[] args)
+    {
+        Console.Write("Digite a cotação do dolar:")
+        double cotação = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\n Digite a quantidade de dolars que se quer comprar: ");
+        double valor = double.Parse(Console.ReadLine());
+
+        double total = Conversor.ConversorProReal(valor, cotação);
+        Console.WriteLine("\n O Valor a ser pago em reais é: " + total.ToString("F2"));
+
+    }
+}
