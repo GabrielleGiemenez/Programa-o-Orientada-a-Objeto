94c49a9f98c754c7a8a6287ab6f32f651561dafb
Gabi <Gabi@DESKTOP-K0ODT6V>
2020-10-30 21:12:28 -0300

    Codigo

---
+++ Encapsulamento e Properties/Encapsulamento e Properties/Produto.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Encap_properties
+{
+    class Produto
+    {
+        string _nome;
+        double _preco;
+        int _quantidade;
+
+        public Produto()
+        {
+        }
+
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+
+        }
+      
+        public double Preco
+        {
+            get { return _preco; }
+        }
+
+        public int Qtde
+        {
+            get { return _quantidade; }
+        }
+        
+
+        public double ValorTotalEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionaProduto(int qtde)
+        {
+            _quantidade += _quantidade;
+        }
+
+        public void RemoveProduto(int qtde)
+        {
+           _quantidade -= _quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome
+                + ", R$ "
+                + _preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + _quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Encapsulamento e Properties/Encapsulamento e Properties/Program.cs
@@ -0,0 +1,40 @@
+﻿using Encap_properties;
+using System;
+using System.Globalization;
+
+namespace Encapsulamento_e_Properties
+
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+          
+                Console.WriteLine("Digite os dados do produto: ");
+                Console.WriteLine("nome");
+                string nome = Console.ReadLine();
+                Console.WriteLine("");
+                Console.WriteLine("Preço: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine("");
+                Console.WriteLine("Quantidade: ");
+                int qtde = int.Parse(Console.ReadLine());
+                Produto produto = new Produto(nome, preco, qtde);
+
+                Console.WriteLine("Dados do produto: " + produto + "\n");
+
+                Console.WriteLine("Coloque o valor que será adicionado ao produto: ");
+                int _quantidade = int.Parse(Console.ReadLine());
+                produto.AdicionaProduto(_quantidade);
+                Console.WriteLine("Produto Final: " + produto + "\n");
+
+        
+                Console.WriteLine("Coloque o valor que será removido do produto: ");
+                _quantidade = int.Parse(Console.ReadLine());
+                produto.RemoveProduto(_quantidade);
+                Console.WriteLine("Produto Final: " + produto + "\n");
+
+            }
+        }
+    }
+}
